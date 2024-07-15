c5fa5bfea1c966d6cc8ff8c3f829f833ff67506f
tetriv0217 <tetriv0217@gmail.com>
2024-07-15 06:15:13 +0530

    Connect to database

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+/** 
+ require("dotenv").config({ path: "./env" });
+ import express from "express";
+const app = express();
+
+
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error",(error)=>{
+        console.log("ERRR:", error);
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App listening at port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log("ERROR : Db error", error);
+    throw error;
+  }
+})();
+*/
